1cc0f746db786dab750c0f21734a1a580304bb06
Jake Flynn <78702792+JakeFlynn19@users.noreply.github.com>
2021-07-27 15:16:08 -0400

    first commit

---
+++ app/src/main/java/com/example/olaytasks/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.olaytasks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(applicationContext, "Welcome", Toast.LENGTH_LONG).show()
+    }
+
+}
